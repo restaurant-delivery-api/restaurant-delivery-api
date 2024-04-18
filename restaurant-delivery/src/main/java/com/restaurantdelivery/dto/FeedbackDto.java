a9cce117c4bd6eda70d1f91960df49006154d95d
DLaaren <dlaaren@yandex.ru>
2024-04-18 15:56:24 +0700

    [FEAT]: added controller&service&repository for feedbacks

---
+++ restaurant-delivery/src/main/java/com/restaurantdelivery/dto/FeedbackDto.java
@@ -0,0 +1,18 @@
+package com.restaurantdelivery.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FeedbackDto {
+    private long id;
+    private int rate;
+    private String comment;
+}
