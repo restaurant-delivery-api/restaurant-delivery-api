7617386ebd576cc635316d7abebdcff9cd3d7c11
d3adl1n3 <ikateshov@gmail.com>
2024-05-15 12:45:40 +0700

    User, Roles, Auth

---
+++ restaurant-delivery/src/main/java/com/restaurantdelivery/dto/UserCreateDto.java
@@ -0,0 +1,23 @@
+package ru.poolnsk.pool.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserCreateDto {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Surname is required")
+    private String surname;
+
+    @NotBlank(message = "Email is required")
+    @Email
+    private String email;
+
+    @Size(min=8, message = "Password is too short min 8 chars")
+    private String password;
+}
