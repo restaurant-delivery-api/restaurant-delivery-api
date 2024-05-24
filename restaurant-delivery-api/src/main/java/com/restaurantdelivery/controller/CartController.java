e0d9b937405d748da82196cf267523f77e6d798d
Olesia <o.palchunova@g.nsu.ru>
2024-05-24 15:32:16 +0700

    create api for cart

--- restaurant-delivery-api/src/main/java/com/restaurantdelivery/controller/CartController.java
+++ restaurant-delivery-api/src/main/java/com/restaurantdelivery/controller/CartController.java
@@ -1,4 +1,4 @@
-package ru.poolnsk.pool.controller;
+package com.restaurantdelivery.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,30 +8,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import ru.poolnsk.pool.dto.CatalogDto;
-import ru.poolnsk.pool.entity.Cart;
-import ru.poolnsk.pool.entity.Category;
-import ru.poolnsk.pool.entity.Product;
-import ru.poolnsk.pool.entity.Section;
-import ru.poolnsk.pool.repository.CartRepository;
-import ru.poolnsk.pool.service.CartService;
-import ru.poolnsk.pool.service.ProductService;
-import ru.poolnsk.pool.service.SectionService;
-import ru.poolnsk.pool.service.UserService;
-import ru.poolnsk.pool.utils.Catalog;
+import com.restaurantdelivery.dto.CatalogDto;
+import com.restaurantdelivery.entity.Cart;
+import com.restaurantdelivery.entity.Category;
+import com.restaurantdelivery.entity.Product;
+import com.restaurantdelivery.entity.Section;
+import com.restaurantdelivery.repository.CartRepository;
+import com.restaurantdelivery.service.CartService;
+import com.restaurantdelivery.service.ProductService;
+import com.restaurantdelivery.service.SectionService;
+import com.restaurantdelivery.service.UserService;
+import com.restaurantdelivery.utils.Catalog;
 
 import java.util.*;
 
 @Controller
 @ResponseStatus(HttpStatus.OK)
 @RequiredArgsConstructor
-public class CartController {
+public class CartController implements CartApi{
     private final ProductService productService;
     private final CartService cartService;
     private final SectionService sectionService;
 
-    @PostMapping("/addToCart/{productId}-{num}")
-    public void addToCart(@PathVariable("productId") String productId, @PathVariable("num") String num, HttpServletRequest request, HttpServletResponse response) {
+    public void addToCart(String productId, String num, HttpServletRequest request, HttpServletResponse response) {
         Cart cart = cartService.getOrCreateCart(request, response);
         Product product = productService.getById(Long.parseLong(productId));
         for (int i = 0; i<Long.parseLong(num); i++) {
@@ -40,8 +39,7 @@ public class CartController {
         cartService.save(cart);
     }
 
-    @PostMapping("/deleteFromCart/{productId}")
-    public void deleteFromCart(@PathVariable("productId") String productId, HttpServletRequest request, HttpServletResponse response) {
+    public void deleteFromCart(String productId, HttpServletRequest request, HttpServletResponse response) {
         Cart cart = cartService.getOrCreateCart(request, response);
         Product product = productService.getById(Long.parseLong(productId));
         while (cart.getProducts().contains(product)) {
@@ -50,23 +48,20 @@ public class CartController {
         cartService.save(cart);
     }
 
-    @PostMapping("/decreaseAmount/{productId}")
-    public void decreaseAmount(@PathVariable("productId") String productId, HttpServletRequest request, HttpServletResponse response) {
+    public void decreaseAmount(String productId, HttpServletRequest request, HttpServletResponse response) {
         Cart cart = cartService.getOrCreateCart(request, response);
         Product product = productService.getById(Long.parseLong(productId));
         cart.deleteProduct(product);
         cartService.save(cart);
     }
 
-    @PostMapping("/increaseAmount/{productId}")
-    public void increaseAmount(@PathVariable("productId") String productId, HttpServletRequest request, HttpServletResponse response) {
+    public void increaseAmount(String productId, HttpServletRequest request, HttpServletResponse response) {
         Cart cart = cartService.getOrCreateCart(request, response);
         Product product = productService.getById(Long.parseLong(productId));
         cart.addProduct(product);
         cartService.save(cart);
     }
 
-    @GetMapping("/cart")
     public ModelAndView viewCart(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView("cart");
         Cart cart = cartService.getOrCreateCart(request, response);
